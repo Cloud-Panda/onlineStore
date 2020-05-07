889be19dc1c9071770c58ec9e6bba6833b266fbd
Abigail <abbyliguma@gmail.com>
2020-05-07 14:33:34 +0200

    javascript added into index for nav bar

--- index.php
+++ index.php
@@ -10,25 +10,38 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
   <body>
-      <head>
+      <header>
           <div class="cover-container">
               <h1 id="title">Saint's</h1>
               <div class="sidebar" id="mySidebar">
+                  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
                   <a href="#">About</a>
                   <a href="#">Login</a>
                   <a href="#">Signup</a>
                   <a href="#">Contact</a>
               </div>
               <div id="main">
-                  <button class="openbtn">Menu</button>
+                  <button class="openbtn" onclick="openNav()">Menu</button>
               </div>
               <h2 id="slogan">Put a slogan here</h2>
           </div>
+        </header>
       
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+    <script>
+        function openNav() {
+      document.getElementById("mySidebar").style.width = "250px";
+      document.getElementById("main").style.marginLeft = "250px";
+    }
+    
+    function closeNav() {
+      document.getElementById("mySidebar").style.width = "0";
+      document.getElementById("main").style.marginLeft= "0";
+    }
+    </script>
+</body>
 </html>
