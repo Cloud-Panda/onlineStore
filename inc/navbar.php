9c4a7c4f1c9192d939cd270a6419d8429eb59513
Abigail <abbyliguma@gmail.com>
2020-05-11 14:13:23 +0200

    bottom nav bar added

--- inc/navbar.php
+++ inc/navbar.php
@@ -8,17 +8,31 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
+    <link rel="stylesheet" type="text/css" href="css/navbarStyle.css">
+</head>
   <body>
   <nav>
          <ul id="top-nav">
              <li id="pick">Woman</li>
              <li id="pick">Men</li>
-             <li id="heading"><h1>Saint's</li></h1>
-             <li id="icon"><img src="2.png" id="icons"></li>
-             <li id="icon"><img src="6.png" id="icons"></li>
-             <li id="icon"><img src="1.png" id="icons"></li>
-             <li id="icon"><img src="5.png" id="icons"></li>
+             <li id="heading">Saint's</li>
+             <li id="icon"><img src="images/2.png" id="icons"></li>
+             <li id="icon"><img src="images/6.png" id="icons"></li>
+             <li id="icon"><img src="images/1.png" id="icons"></li>
+             <li id="icon"><img src="images/5.png" id="icons"></li>
+         </ul>
+     </nav>
+     <!--Second Nav-->
+     <nav>
+         <ul id="bottom-nav">
+             <li>Tops</li>
+             <li>Jeans</li>
+             <li>Jackets</li>
+             <li>Bottoms</li>
+             <li>Swimsuits</li>
+             <li>Dresses</li>
+             <li>Accessories</li>
+             <li>Shoes</li>
          </ul>
      </nav>
     <!-- Optional JavaScript -->
