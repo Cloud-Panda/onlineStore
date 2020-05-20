b1db0e60a4925c3286c80561f22c2fa4b20e27f6
Abigail <abbyliguma@gmail.com>
2020-05-20 23:46:17 +0200

    images added, div names changed, navbar fixed

--- index.php
+++ index.php
@@ -1,49 +1,60 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>Title</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/landingStyle.css" type="text/css">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="css/landingStyle.css">
 </head>
-  <body>
-      <nav>
+<body>
+     <nav>
           <ul class="navbar">
-              <li>Logo</li>
+              <li id="logo">SAINT'S</li>
               <li>SignUp/Login</li>
               <li>Women</li>
               <li>Men</li>
-              <li>Your Saint's</li>
-              <li>Instagram</li>
-              <li>Facebook</li>
-              <li>Twitter</li>
-              <li>Snapchat</li>
-              <li>Pinterest</li>
+              <li>Plus size</li>
+              <li id="socials"><img src="images/instagram.png" id="icon"></li>
+              <li><img src="images/twitter.png" id="icon"></li>
+              <li><img src="images/pin.png" id="icon"></li>
+              <li><img src="images/snap.png" id="icon"></li>
+              <li><img src="images/facebook.png" id="icon"></li>
           </ul>
       </nav>
-      <div class="container">
-          <div class="Coming-soon">
-              <img src="images/26 (5).jpg" alt="image">
-          </div>
-          <div class="women">
-              <img src="images/24 (4).jpg" alt="image">
-              <div class="men">
-                <img src="images/29 (3).jpg" alt="image">
-          </div>
-          </div>
-          <div class="your-saint">
-              <img src="images/27 (6).jpg" alt="image">
-          </div>
-      </div>
-      
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+      <div class="App">
+           
+        <div class="first">
+
+        </div>
+        <div class="mid">
+            <div class="top">
+            </div>
+            <div class="bottom">
+            </div>
+        </div>
+        <div class="last">
+        </div>
+    </div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos at aliquid quibusdam fuga fugit! Ab soluta, 
+    animi pariatur laboriosam ullam necessitatibus eveniet, rerum facilis laudantium repellendus laborum doloribus aspernatur temporibus?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos at aliquid quibusdam fuga fugit! Ab soluta, 
+    animi pariatur laboriosam ullam necessitatibus eveniet, rerum facilis laudantium repellendus laborum doloribus aspernatur temporibus?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos at aliquid quibusdam fuga fugit! Ab soluta, 
+    animi pariatur laboriosam ullam necessitatibus eveniet, rerum facilis laudantium repellendus laborum doloribus aspernatur temporibus?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos at aliquid quibusdam fuga fugit! Ab soluta, 
+    animi pariatur laboriosam ullam necessitatibus eveniet, rerum facilis laudantium repellendus laborum doloribus aspernatur temporibus?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos at aliquid quibusdam fuga fugit! Ab soluta, 
+    animi pariatur laboriosam ullam necessitatibus eveniet, rerum facilis laudantium repellendus laborum doloribus aspernatur temporibus?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos at aliquid quibusdam fuga fugit! Ab soluta, 
+    animi pariatur laboriosam ullam necessitatibus eveniet, rerum facilis laudantium repellendus laborum doloribus aspernatur temporibus?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos at aliquid quibusdam fuga fugit! Ab soluta, 
+    animi pariatur laboriosam ullam necessitatibus eveniet, rerum facilis laudantium repellendus laborum doloribus aspernatur temporibus?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos at aliquid quibusdam fuga fugit! Ab soluta, 
+    animi pariatur laboriosam ullam necessitatibus eveniet, rerum facilis laudantium repellendus laborum doloribus aspernatur temporibus?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos at aliquid quibusdam fuga fugit! Ab soluta, 
+    animi pariatur laboriosam ullam necessitatibus eveniet, rerum facilis laudantium repellendus laborum doloribus aspernatur temporibus?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos at aliquid quibusdam fuga fugit! Ab soluta, 
+    animi pariatur laboriosam ullam necessitatibus eveniet, rerum facilis laudantium repellendus laborum doloribus aspernatur temporibus?
+    
+</body>
 </html>
