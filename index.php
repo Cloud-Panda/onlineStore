0f1f462df56a1d5a6d4a0b5e6062aab199f96e56
Abigail <abbyliguma@gmail.com>
2020-05-20 17:28:58 +0200

    New nav bar added/ start of new design

--- index.php
+++ index.php
@@ -8,64 +8,30 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!--My css-->
-    <link rel="stylesheet" type="text/css" href="css/landingStyle.css">
+    <link rel="stylesheet" href="css/landingStyle.css" type="text/css">
 </head>
-
-<body>
-    <div class="main">
-        <!--Left panel-->
-        <div class="left">
-            <div class="left-nav">
-                <ul class="right-navbar">
-                  <li>About</li>
-                  <li>Contact</li>
-                  <li>SignUp/Login</li>
-                </ul>
-            </div>
-        </div>
-
-        <!--Right Panel-->
-        <div class="right">
-
-            <!--Right Navbar-->
-            <div class="right-top">
-                <ul class="right-navbar">
-                  <li>Woman</li>
-                  <li>Men</li>
-                  <li><img src="images/30.png" alt="search icon" id="icon"></li>
-                </ul>
-            </div>
-
-            <!--Middel Box-->
-            <div class="right-bottom">
-                <h1 id="saint">Saint</h1>
-                <image src="images/29 (2).jpg" id="hero">
-                    <img src="images/4.jpg" id="hero">
-                </div>
-
-                <!--Right box-->
-                <div class="left-top">
-                    <h1 id="saint2">Saint</h1>
-                </div>
-                
-            </div>
-        </div>
-        <div class="upcoming">
-            Up Coming
-        </div>
-        <div class="featured">
-            Featured
-        </div>
-        <div class="Footer">
-            Footer
-        </div>
-        
+  <body>
+      <nav>
+          <ul class="nav-bar">
+              <li>Logo</li>
+              <li>SignUp/Login</li>
+              <li>Women</li>
+              <li>Men</li>
+              <li>Your Saint's</li>
+          </ul>
+          <ul class="socials">
+              <li>Instagram</li>
+              <li>Facebook</li>
+              <li>Twitter</li>
+              <li>Snapchat</li>
+              <li>Pinterest</li>
+          </ul>
+      </nav>
+      
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    </script>
-</body>
+  </body>
 </html>
