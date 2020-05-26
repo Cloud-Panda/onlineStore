a430a2dbc64ded576bdb3dd8526234e905083eb4
Abigail <abbyliguma@gmail.com>
2020-05-27 00:14:52 +0200

    gallery added

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" type="text/css" href="css/landingStyle.css">
 </head>
   <body>
-      <div class="covid"><p id="covid-txt">COVID-19 BE SAFE AND STAY HOME !!! </p></div>
+      <div class="covid"><p id="covid-txt">STAY HOME SAVE LIVES STOP COVID-9 !! </p></div>
     <nav>
         <ul class="navbar">
             <li><button>SignUp/Login</button></li>
@@ -106,46 +106,19 @@
       </div>
 
       <div class="Featured">
-          <hr><h3>Customer Favourites</h3>
+          <h3>Customer Favourites</h3>
           <div class="features">Img here</div>
           <div class="features">img here</div>
           <div class="features">img here</div>
           <div class="features">img here</div>
       </div>
 
-      <div class="footer">
-          <div><h2>Saint's</h2>
-          <p id="footer-p">We supply stunning <br>designs for your home</p>
-            </div>
-          <ul id="footer">
-              <li><b>EXPLORE</b></li>
-              <li>Home</li>
-              <li>About</li>
-              <li>Contact</li>
-              <li>Gallery</li>
-          </ul>
-          <ul id="footer">
-              <li><b>VISIT</b></li>
-              <li>Map Location</li>
-              <li>26 Poort Road<br>America 7798
-              </li>
-          </ul>
-          <ul id="footer">
-              <li><b>FOLLOW</b></li>
-              <li>Facebook</li>
-              <li>Insatgram</li>
-              <li>Twitter</li>
-              <li>Snapchat</li>
-          </ul>
-          <ul id="footer">
-              <li><b>LEGAL</b></li>
-              <li>Terms</li>
-              <li>Privacy</li>
-          </ul>
-          <a class="gotopbtn" href="#"> <i class="fas fa-arrow-up"></i><img src="images/upload.png" id="up"></a>
+      <div class="gallery">
           
       </div>
-      <p id="rights">&copy;  Code Space | Designed by Abigail.L</p>
+
+      <?php include("inc/footer.php");?>
+
       
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
